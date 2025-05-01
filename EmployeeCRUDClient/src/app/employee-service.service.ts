198a7e6ef84d9e56c7a532d6470cd723ddd837df
0x0desha74 <mudesha74@gmail.com>
2025-05-01 22:44:43 +0300

    update employee

--- EmployeeCRUDClient/src/app/employee-service.service.ts
+++ EmployeeCRUDClient/src/app/employee-service.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EmployeeServiceService {
+ 
 
   baseURL = "https://localhost:7248/api/employees"
 
@@ -16,8 +17,19 @@ export class EmployeeServiceService {
     return this.httpClient.post(`${this.baseURL}`, employee);
   }
 
+  getEmployeeById(id:number): Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+   }
+
 
 getEmployees():Observable<Employee[]>{
   return this.httpClient.get<Employee[]>(`${this.baseURL}`);
 }
+
+updateEmployeeData(employee: Employee,id:number): Observable<Employee> {
+ return this.httpClient.put<Employee>(`${this.baseURL}/${id}`,employee);
+
+}
+
+
 }
--- EmployeeCRUDClient/src/app/employee-update/employee-update.component.ts
+++ EmployeeCRUDClient/src/app/employee-update/employee-update.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../employee';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeServiceService } from '../employee-service.service';
 
 @Component({
   selector: 'app-employee-update',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee-update.component.html',
   styleUrl: './employee-update.component.css'
 })
 export class EmployeeUpdateComponent {
+
+  id!: number;
+  employee: Employee = new Employee();
+
+  constructor(
+    private activeRouter: ActivatedRoute,
+    private employeeService: EmployeeServiceService,
+    private router : Router
+  ) { }
+
+  ngOnInit(): void {
+    this.id = this.activeRouter.snapshot.params['id'];
+    console.log(this.id);
+    this.employeeService.getEmployeeById(this.id).subscribe(
+      data => {
+        this.employee = data;
+      })
+  }
+
+  updateEmployeeData() {
+this.employeeService.updateEmployeeData(this.employee,this.id).subscribe(
+  data =>{
+  alert("Employee updated successfully");
+  this.router.navigate(['/']);
+},
+error =>{
+  alert("Failed to update the Employee");
+})
+  }
 
 }
